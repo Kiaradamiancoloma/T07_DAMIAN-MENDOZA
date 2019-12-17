e3b949c88da8ae6d124fb0307aebf6d2be7e11a1
Kiaradamiancoloma <57924219+kiaradamiancoloma@users.noreply.github.com>
2019-12-16 22:38:29 -0500

    Termino de commit

---
+++ lote.bat
@@ -0,0 +1,42 @@
+python DAMIAN_COLOMA/iteracion1.py  "#"
+python  DAMIAN_COLOMA/iteracion1.py  "$"
+python  DAMIAN_COLOMA/iteracion1.py  "O"
+
+python  DAMIAN_COLOMA/iteracion2.py  "A"
+python  DAMIAN_COLOMA/iteracion2.py  "B"
+python  DAMIAN_COLOMA/iteracion2.py  "C"
+
+python  DAMIAN_COLOMA/iteracion3.py  "Q"
+python  DAMIAN_COLOMA/iteracion3.py "S"
+python  DAMIAN_COLOMA/iteracion3.py "K"
+
+python  DAMIAN_COLOMA/iteracion4.py "R"
+python  DAMIAN_COLOMA/iteracion4.py "B"
+python  DAMIAN_COLOMA/iteracion4.py "E"
+
+python  DAMIAN_COLOMA/iteracion5.py "B"
+python  DAMIAN_COLOMA/iteracion5.py "D"
+python  DAMIAN_COLOMA/iteracion5.py "M"
+
+
+
+
+python Mendoza/iteracion1.py "HOLA"
+python Mendoza/iteracion1.py "DIME"
+python Mendoza/iteracion1.py "CHAU"
+
+python Mendoza/iteracion2.py 0
+python Mendoza/iteracion2.py 1
+python Mendoza/iteracion2.py 2
+
+python Mendoza/iteracion3.py "T"
+python Mendoza/iteracion3.py "I"
+python Mendoza/iteracion3.py "A"
+
+python Mendoza/iteracion4.py "DESAYUNO"
+python Mendoza/iteracion4.py "ALMUERZO"
+python Mendoza/iteracion4.py "CENA"
+
+python Mendoza/iteracion5.py 109 76
+python Mendoza/iteracion5.py 3 5
+python Mendoza/iteracion5.py 45 45
